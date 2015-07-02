53e8c807e4bce78a7c7e0810bdb6bb460fc8c212
Sam Etheridge <sam@catch.co.nz>
2015-07-02 14:44:00 +1200

    such things

---
+++ droposaurus.js
@@ -0,0 +1,273 @@
+;
+(function($, window, document, undefined) {
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = "catchDropdown",
+        defaults = {
+            propertyName: "value"
+        };
+
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        this.element = element;
+        this.jqElem = $(element);
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        if (!this.jqElem.is('.dropasaurusised')) this.init();
+    }
+
+    Plugin.prototype = {
+        init: function() {
+
+            // vars
+            var $el = this.jqElem,
+                self = this;
+
+            // touch my body
+            $.addBody();
+
+            // iterate through each elem in the jq selector
+            $el.each(function() {
+
+                var $el = $(this).wrap('<div class="dropdown-wrapper' + ($(this).data('size') ? (' size'+$(this).data('size')) : '') +'"></div>'),
+                    $par = $el.parent(),
+                    classList = ($el.attr('class') || '').replace('select-invisible', ''),
+                    catchDropdownHtml = '<label class="' + ($el.data('error ') ? 'error' : '') + classList + '"><span class="span-label' + ($el.data('hideLabel') ? ' hidden' : '') + '">' + (!$el.data('mobile-only-label') && $el.data('label') || '') + '</span>' +
+                                            '<div class="btn-dd">' +
+                                                '<a href="" class="input btn-dd-select phone-type icon-chevron-fat-down' + ($el.data('selected') ? ' populated' : '') + '" tabindex="0">' +
+                                                '<span class="main"'+
+                                                    ( $el.data('placeholder-color') ? (' style="color:'+$el.data('placeholder-color')+'"') : '' )+
+                                                    '>' + $el.data('placeholder') + '</span>' +
+                                                ($el.data('subtext') ? ("<span class='sub'>" + $el.data('subtext') + "</span>") : "") + '</a>' +
+                                                '<div class="btn-dd-options">' +
+                                                    '<div class="btn-dd-header">' +
+                                                        $el.data('label') +
+                                                        ' <a class="btn-dd-close">Close</a>' +
+                                                    '</div>' +
+                                                    '<ul></ul>' +
+                                                '</div>' +
+                                            '</div>' +
+                                        '</label>'
+
+                //set up the proper display of the select and list elements
+                $el.after(catchDropdownHtml);
+                $el.addClass('hidden');
+
+                // update the options
+                self.update();
+
+                // attach the gusher
+                $par.find('ul').gush({
+                    x: false,
+                    move: {
+                        stopPropagation: true
+                    }
+                });
+
+                //When clicking off the menu, close the menu
+                $('html').click(function(e) {
+                    $par.find('.btn-dd').removeClass('active');
+                    $('.body').removeClass('no-overflow');
+                });
+
+                //tab, enter, arrow keys
+                var focused = -1
+                $(document).on('keydown.dropasaur', function(e) {
+                    //down = 40
+                    if (e.keyCode == 40) {
+                        if ($par.find('.btn-dd.active').length) {
+                            e.preventDefault();
+                            var options = $par.find('.btn-dd-option a');
+                            if (focused < options.length - 1) {
+                                focused++;
+                            }
+                            var focusedElem = options[focused]
+                            focusedElem.focus()
+                            $el.find('option[value="' + $(focusedElem).data('value') + '"]').prop('selected', true);
+                            $el.trigger('change');
+                        }
+                    }
+                    //up = 38
+                    else if (e.keyCode == 38) {
+                        if ($par.find('.btn-dd.active').length) {
+                            e.preventDefault();
+                            var options = $par.find('.btn-dd-option a');
+                            if (focused > 0) {
+                                focused--;
+                            }
+                            var focusedElem = options[focused]
+                            focusedElem.focus()
+                            $el.find('option[value="' + $(focusedElem).data('value') + '"]').prop('selected', true);
+                            $el.trigger('change');
+                        }
+                    }
+                    //tab
+                    else if (e.keyCode == 9) {
+                        var active = $par.find('.btn-dd.active');
+                        if (active.length) {
+                            var $sel = $el.find('option[value="' + $el.val() + '"]');
+                            e.preventDefault();
+                            active.removeClass('active');
+                            $('.body').removeClass('no-overflow');
+                            active.find('a').first().addClass('populated');
+                            active.find('a').first().focus();
+                            active.find('a .main').first().text($sel.text());
+                            if (active.find('a .sub').length == 0) {
+                                active.find('a').append('<span class="sub"></span>')
+                            }
+                            active.find('a .sub').first().text($el.data('subtext') || $sel.data('subtext') || '');
+                        }
+                    }
+                    //enter
+                    else if (e.keyCode == 13) {
+                        var active = $par.find('.btn-dd.active');
+                        if (active.length) {
+                            var $sel = $el.find('option[value="' + $el.val() + '"]');
+                            e.preventDefault();
+                            active.removeClass('active');
+                            $('.body').removeClass('no-overflow');
+                            active.find('a').first().addClass('populated');
+                            active.find('a').first().focus();
+                            active.find('a .main').first().text($sel.text());
+                            if (active.find('a .sub').length == 0) {
+                                active.find('a').append('<span class="sub"></span>')
+                            }
+                            active.find('a .sub').first().text($el.data('subtext') || $sel.data('subtext') || '');
+                        }
+                    }
+                });
+            });
+
+            // add the class so we know its all initialised
+            this.jqElem.addClass('dropasaurusised');
+        },
+
+        update: function(params) {
+
+            var $el = this.jqElem,
+                $sel,
+                opt,
+                i;
+
+            $el.each(function(){
+
+                var $el = $(this),
+                    $par = $el.closest('.dropdown-wrapper');
+
+                // update the original select if we have new options provided
+                if (typeof params == 'object') {
+
+                    $el.html('');
+
+                    for (i=0; i<params.length; i++) {
+                        opt = params[i];
+                        $el.append('<option value="' + opt.value + '" data-subtext="' + opt.subtext + '">' + (opt.innerHTML ? opt.innerHTML : opt.value) + '<option>');
+                    }
+
+                } else if (typeof params == 'string') {
+
+                    $el.html(params);
+
+                }
+
+                if (params != undefined) $par.find('ul').html('');
+
+                //Add the blank option if allowed
+                $el.prepend('<option value=""></option>');
+                if ($el.attr('allow-empty') == "true") {
+                    $par.find('ul').append('<li class="input btn-dd-option catch-dropdown-item"><a class="catch-dropdown-link" href="" tabindex="-1" data-value="">&nbsp;</a></li>')
+                }
+
+                //populate the list with the select items
+                // console.log($el.find('option'));
+                $el.find('option').each(function() {
+                    if ($(this).val() != '') {
+                        $par.find('ul').append('<li class="input btn-dd-option catch-dropdown-item"><a class="catch-dropdown-link' + ($(this).text() == 'Add Connection' ? ' btn--add btn--icon icon-add icon-after' : '') + ($el.data('selected') == $(this).val() ? ' selected' : '') + '" href="" tabindex="-1" data-value="' + $(this).val() + '">' +
+                            '<span class="main">' + $(this).text() + '</span>' +
+                            ($(this).data('subtext') ? '<span class="sub">' + $(this).data('subtext') + '</span>' : '') +
+                            '</a></li>')
+                    }
+                });
+
+                //Preselect an option if one is specified, else the first
+                $sel = $el.data('selected')!=null ? $el.find('option[value="' + $el.data('selected') + '"]') : $el.find('option').first();
+                $sel.prop('selected', true);
+                $el.trigger('change');
+                $par.find('.btn-dd-select .main').first().text($sel.text() || $el.data('placeholder') || '&nbsp;');
+                if ($par.find('.btn-dd-select .sub').length == 0) {
+                    $par.find('.btn-dd-select').append('<span class="sub"></span>')
+                }
+                $par.find('.btn-dd-select .sub').first().text($el.data('subtext') || $sel.data('subtext') || '');
+
+                //When clicking on the outer button thing, make it open and close the menu
+                $par.find('.btn-dd').off('click.dropasaur').on('click.dropasaur', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if($(this).hasClass('disabled')){
+                        return;
+                    }
+                    if (!$(this).hasClass('active')) {
+                        $('.btn-dd').removeClass('active');
+                    }
+                    $el.trigger('probable_change');
+                    var selected = $el.val();
+                    $(this).find('li a').each(function() {
+                        $(this).toggleClass('selected', selected == $(this).data('value'));
+                    });
+                    $(this).toggleClass('active');
+                    $('.body').toggleClass('no-overflow');
+                    $(this).find('a').first().addClass('populated');
+                });
+
+                //When clicking on the menu items, select that menu item and close the menu
+                $par.find('li a').off('click.dropasaur').on('click.dropasaur', function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    var $sel = $el.find('option[value="' + $(this).data('value') + '"]').prop('selected', true);
+                    $par.find('.btn-dd-select .main').first().text($(this).find('.main').text());
+                    if ($par.find('.btn-dd-select .sub').length == 0) {
+                        $par.find('.btn-dd-select').append('<span class="sub"></span>')
+                    }
+                    $par.find('.btn-dd-select .sub').first().text($el.data('subtext') || $sel.data('subtext') || '');
+                    $el.trigger('change');
+                    $('.body').removeClass('no-overflow');
+                    $par.find('.btn-dd').toggleClass('active').find('a').first().addClass('populated');
+                });
+
+            });
+
+        },
+        setDisabled: function(disabled){
+            this.jqElem.parent().find('.btn-dd').toggleClass('disabled',disabled);
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function(options, params) {
+        return this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            } else if (options == 'update') {
+                $.data(this, "plugin_" + pluginName).update(params);
+            } else if (options == 'setDisabled') {
+                $.data(this, "plugin_" + pluginName).setDisabled(params);
+            }
+        });
+    };
+
+})(jQuery, window, document);
---
+++ simple-dropdown.js
@@ -0,0 +1,200 @@
+;(function ( $, window, document, undefined ) {
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = "simpleDropdown",
+    defaults = {
+        propertyName: "value"
+    };
+
+    // The actual plugin constructor
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.jqElem = $(element);
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        if (!this.jqElem.is('.dropasaurusised, .simple-dd-generated')) this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+
+            // Place initialization logic here
+            // You already have access to the DOM element and
+            // the options via the instance, e.g. this.element
+            // and this.options
+            // you can add more functions like the one below and
+            // call them like so: this.yourOtherFunction(this.element, this.options).
+
+            // touch my body
+            $.addBody();
+
+            var el = this.jqElem;
+            var outEl = this.jqElem;
+
+            el.each(function(){
+                var el = $(this);
+
+                var w = el.find('ul');
+
+                var y = w.parent();
+
+                var x = w.remove();
+
+                $('.body').append(x);
+
+                x.wrap(
+                    '<div class="simple-dd simple-dd-generated ' + (el.is('.pin-right') ? 'pin-right' : '') + '"' +
+                    '     id="' + el.data('id') + '"></div>'
+                );
+
+                $(window).on('resize',function(){
+
+                    var conf = {
+                        position: 'absolute',
+                        top: y.offset().top + y.height(),
+                    }
+
+                    if (el.is('.pin-right')) {
+                        conf['left'] = y.offset().left - x.outerWidth() + y.outerWidth();
+                    } else {
+                        conf['left'] = y.offset().left;
+                    }
+
+                    x.parent().css(conf);
+                });
+
+
+                var focused = -1;
+
+                //When clicking on the outer button thing, make it open and close the menu
+                el.find('a').first().click(function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    // console.log(outEl)
+                    focused = -1;
+                    if($('#'+el.data('id')).hasClass('active')){
+                        $('.simple-dd').removeClass('active');
+                        outEl.removeClass('active');
+                        return
+                    }
+                    $('.simple-dd').removeClass('active');
+                    el.trigger('probable_change');
+                    $('#'+el.data('id')).toggleClass('active');
+                    outEl.toggleClass('active');
+
+                    $(window).trigger('resize');
+                })
+
+                //When clicking off the menu, close the menu
+                $('html').click(function(e){
+                    $('.simple-dd').removeClass('active');
+                })
+
+                //tab, enter, arrow keys
+                var shiftDown = false;
+                $(document).keyup(function(e){
+                    if(e.keyCode == 16){
+                        shiftDown=false;
+                    }
+                });
+                $(document).keydown(function(e){
+                    //down = 40
+                    if(e.keyCode == 16){
+                        shiftDown=true;
+                    }
+                    else if(e.keyCode == 40){
+                        if($('#'+el.data('id')).hasClass('active')){
+                            e.preventDefault();
+                            var options = $('#'+el.data('id')).find('a');
+                            if(focused < options.length-1){ focused++; }
+                            var focusedElem = options[focused];
+                            focusedElem.focus();
+                        }
+                    }
+                    //tab = 9
+                    else if(e.keyCode == 9){
+                        if($('#'+el.data('id')).hasClass('active')){
+                            var options = $('#'+el.data('id')).find('a');
+                            if(focused < options.length-1 && !shiftDown){
+                                focused++;
+                                var focusedElem = options[focused];
+                                focusedElem.focus();
+                                e.preventDefault();
+                            }
+                            else if(shiftDown){
+                                if(focused > 0){
+                                    focused--;
+                                    var focusedElem = options[focused];
+                                    focusedElem.focus();
+                                }
+                                else {
+                                    focused--;
+                                    $('.simple-dd').removeClass('active');
+                                    el.find('a').first().focus();
+                                }
+                                e.preventDefault();
+                            }
+                            else {
+                                focused++;
+                                if(el.data('next')){
+                                    $(el.data('next')).find('a').focus();
+                                    $('.simple-dd').removeClass('active');
+                                    e.preventDefault();
+                                }
+                                else {
+                                    el.find('a').focus();
+                                    $('.simple-dd').removeClass('active');
+                                }
+                            }
+                        }
+                    }
+                    //up = 38
+                    else if(e.keyCode == 38){
+                        if($('#'+el.data('id')).hasClass('active')){
+                            e.preventDefault();
+                            var options = $('#'+el.data('id')).find('a');
+                            if(focused > 0){ focused--; }
+                            var focusedElem = options[focused];
+                            focusedElem.focus();
+                        }
+                    }
+                    //esc = 27
+                    else if(e.keyCode == 27){
+                        if($('#'+el.data('id')).hasClass('active')){
+                            $('.simple-dd').removeClass('active');
+                        }
+                    }
+                });
+            });
+
+            // add the class so we know its all initialised
+            this.jqElem.addClass('dropasaurusised');
+
+        }
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[ pluginName ] = function ( options ) {
+        return this.each(function() {
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+            }
+        });
+    };
+
+})( jQuery, window, document );
